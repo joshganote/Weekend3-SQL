063580afb36dd0d778648e5763b1ce44d7a24bf6
Josh Ganote <j.ganote0825@gmail.com>
2019-11-28 21:19:31 -0600

    hooked up ajax calls and render function is working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,7 @@ $(document).ready(init);
 
 function init() {
     $('#addToList').on('submit', onSubmitForm);
+    getTask();
 }
 
 function onSubmitForm(event) {
@@ -11,7 +12,54 @@ function onSubmitForm(event) {
     const newTaskObject = {
         task: $('#task').val(),
         tools: $('#tools').val(),
-        complete: $('#complete').val()
-    }
+        complete: $('#complete').val(),
+    };
+
+    postTask(newTaskObject);
     console.log(newTaskObject);
+};
+
+function getTask() {
+    $.ajax({
+        method: 'GET',
+        url: 'api/to-do-app'
+    })
+    .then ((response) => {
+        console.log('GET');
+        render(response);
+    })
+    .catch ((err) => {
+        console.warn(err);
+    })
+};
+function postTask(newTaskObject) {
+    $.ajax({
+        method: 'POST',
+        url: 'api/to-do-app',
+        data: newTaskObject
+    })
+    .then ((response) => {
+        console.log('POST');
+        getTask();
+    })
+    .catch ((err) => {
+        console.warn(err);
+    })
+};
+
+function render(response) {
+    $('#viewTask').empty();
+
+    for(let i = 0; i < response.length; i++){
+        const toDo = response[i];
+   
+         $('#viewTask').append(`
+         <tr>
+           <td>${toDo.task}</td>
+           <td>${toDo.tools}</td>
+           <td>${toDo.complete}</td>
+         </tr>
+       `)
+     
+    }
 };
