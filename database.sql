e6aa5e4cf2e71c3fe03f678d8ec40a036788d165
Josh Ganote <j.ganote0825@gmail.com>
2019-11-29 01:50:29 -0600

    stuck on render function for 'delete' and 'finished' button

--- database.sql
+++ database.sql
@@ -4,10 +4,11 @@ CREATE TABLE "weekend-to-do-app"(
 	"complete" varchar(100),
 	"status" varchar(100)
 );
--- INSERT INTO "weekend-to-do-app"(task, tools, complete)
--- VALUES('Learn HTML, CSS, and JavaScript', 'VSC, and Prime Instructors', '6 weeks'),
--- ('Learn jQuery, Node.js, React, Mongo, and SQL', 'VSC, and Prime Instructors', '7 weeks'),
--- ('Learn Project Scoping, Team Colab, Tech Research, Client Presentations', 'VSC, and Team Tyto', '7 weeks'),
--- ('Continue Learning Photoshop', 'Creative Live, Ben Wilmore', '12 weeks'),
--- ('Photoshop Compositing', 'Creative Live, Brooke Shaden', '12 weeks'),
--- ('Learn Drop 2 Chord Vocing', 'Jamplay Horace Bray', '6 weeks');
+-- INSERT INTO "weekend-to-do-app"(task, tools, complete, status)
+-- VALUES('Learn HTML, CSS, and JavaScript', 'VSC, and Prime Instructors', '6 weeks', 'Unfinished'),
+-- ('Learn jQuery, Node.js, React, Mongo, and SQL', 'VSC, and Prime Instructors', '7 weeks', 'Unfinished'),
+-- ('Learn Project Scoping, Team Colab, Tech Research, Client Presentations', 'VSC, and Team Tyto', '7 weeks', 'Unfinished'),
+-- ('Continue Learning Photoshop', 'Creative Live, Ben Wilmore', '12 weeks', 'Unfinished'),
+-- ('Photoshop Compositing', 'Creative Live, Brooke Shaden', '12 weeks', 'Unfinished'),
+-- ('Learn Drop 2 Chord Vocing', 'Jamplay, Horace Bray', '6 weeks', 'Unfinished'),
+-- ('Work on LinkedIn Account', 'Rachael Bromander', 'Sunday Night', 'Unfinished');
